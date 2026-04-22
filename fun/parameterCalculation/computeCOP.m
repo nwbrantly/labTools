3cfe1dbea09edb0abcb0faeb28fd8223f9ffc42b
nwb18 <nwbrantly@pitt.edu>
2026-04-22 15:41:30 -0400

    Update function documentation
    Update function documentation comment block using Claude Code to be more comprehensive and include standard sections. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/parameterCalculation/computeCOP.m
+++ fun/parameterCalculation/computeCOP.m
@@ -1,7 +1,4 @@
 function COP = computeCOP(GRFDataS, GRFDataF, s, f)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
 LTransformationMatrix = [1, 0, 0, 0; 20, 1, 0, 0; 1612, 0, -1, 0; 0, 0, 0, -1];
 RTransformationMatrix = [1, 0, 0, 0; -944, -1, 0, 0; 1612, 0, -1, 0; 0, 0, 0, -1];
 eval(['STransformationMatrix=' s 'TransformationMatrix(2:end, 2:end);']);
@@ -23,6 +20,33 @@ COPF(:, 3) = 0;
 COPF = bsxfun(@plus, FTransformationMatrix*COPF', FTransformationVec);
 FzF = relGRF(:, 3);
 COP = bsxfun(@rdivide,(bsxfun(@times,COPF,FzF') + bsxfun(@times,COPS,FzS')),(FzS'+FzF'));
+% computeCOP  Compute bilateral center of pressure in lab coordinates.
+%
+%   Syntax:
+%     COP = computeCOP(grfDataSlow, grfDataFast, slowLeg, fastLeg)
+%
+%   Computes the instantaneous center of pressure (COP) from bilateral
+% ground reaction force (GRF) data. Each leg's COP is computed in
+% force plate coordinates and then transformed to lab coordinates using
+% plate-specific calibration matrices. The combined COP is a vertical-
+% force-weighted average of the two leg COPs.
+%
+%   Inputs:
+%     grfDataSlow - N-by-5 matrix of GRF data for the slow-leg force
+%                   plate [Fx Fy Fz Mx My] in force plate coordinates
+%     grfDataFast - N-by-5 matrix of GRF data for the fast-leg force
+%                   plate [Fx Fy Fz Mx My] in force plate coordinates
+%     slowLeg     - Char 'L' or 'R' identifying the slow-leg plate
+%     fastLeg     - Char 'L' or 'R' identifying the fast-leg plate
+%
+%   Outputs:
+%     COP - 3-by-N matrix of combined COP position in lab coordinates
+%
+%   Toolbox Dependencies:
+%     None
+%
+%   See also: computeForceParameters, computeCOM
+
 
 end
 
