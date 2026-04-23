c7914b27d86bb9b33218335c91a20b9ca183d0da
nwb18 <nwbrantly@pitt.edu>
2026-04-22 22:03:37 -0400

    Reformat function
    Rename function variables using Claude Code to improve function readability, making it more explicit as to what each line of code does. Add clear section headers and code block comments. Update code whitespace for consistency and clarity. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/parameterCalculation/computeCOP.m
+++ fun/parameterCalculation/computeCOP.m
@@ -1,25 +1,4 @@
-function COP = computeCOP(GRFDataS, GRFDataF, s, f)
-LTransformationMatrix = [1, 0, 0, 0; 20, 1, 0, 0; 1612, 0, -1, 0; 0, 0, 0, -1];
-RTransformationMatrix = [1, 0, 0, 0; -944, -1, 0, 0; 1612, 0, -1, 0; 0, 0, 0, -1];
-eval(['STransformationMatrix=' s 'TransformationMatrix(2:end, 2:end);']);
-eval(['FTransformationMatrix=' f 'TransformationMatrix(2:end, 2:end);']);
-eval(['STransformationVec=' s 'TransformationMatrix(2:end, 1);']);
-eval(['FTransformationVec=' f 'TransformationMatrix(2:end, 1);']);
-
-relGRF = GRFDataS;
-COPS(:, 2) = (-5*relGRF(:, 2) + relGRF(:, 4))./relGRF(:, 3);
-COPS(:, 1) = (-5*relGRF(:, 1) - relGRF(:, 5))./relGRF(:, 3);
-COPS(:, 3) = 0;
-COPS = bsxfun(@plus, STransformationMatrix*COPS', STransformationVec);
-
-FzS = relGRF(:, 3);
-relGRF = GRFDataF;
-COPF(:, 2) = (-5*relGRF(:, 2) + relGRF(:, 4))./relGRF(:, 3);
-COPF(:, 1) = (-5*relGRF(:, 1) - relGRF(:, 5))./relGRF(:, 3);
-COPF(:, 3) = 0;
-COPF = bsxfun(@plus, FTransformationMatrix*COPF', FTransformationVec);
-FzF = relGRF(:, 3);
-COP = bsxfun(@rdivide,(bsxfun(@times,COPF,FzF') + bsxfun(@times,COPS,FzS')),(FzS'+FzF'));
+function COP = computeCOP(grfDataSlow, grfDataFast, slowLeg, fastLeg)
 % computeCOP  Compute bilateral center of pressure in lab coordinates.
 %
 %   Syntax:
@@ -53,6 +32,51 @@ arguments
     slowLeg     (1,1) char
     fastLeg     (1,1) char
 end
+
+%% Force Plate Transformation Matrices
+% NOTE: Calibration matrices encode lab-specific force plate positions
+% and orientations on the instrumented treadmill; do not modify.
+LTransformationMatrix = [ ...
+    1,    0,  0,  0; ...
+    20,   1,  0,  0; ...
+    1612, 0, -1,  0; ...
+    0,    0,  0, -1];
+RTransformationMatrix = [ ...
+    1,     0,  0,  0; ...
+    -944, -1,  0,  0; ...
+    1612,  0, -1,  0; ...
+    0,     0,  0, -1];
+
+eval(['slowTransMat=' slowLeg 'TransformationMatrix(2:end, 2:end);']);
+eval(['fastTransMat=' fastLeg 'TransformationMatrix(2:end, 2:end);']);
+eval(['slowTransVec=' slowLeg 'TransformationMatrix(2:end, 1);']);
+eval(['fastTransVec=' fastLeg 'TransformationMatrix(2:end, 1);']);
+
+%% Compute COP for Slow Leg
+% COP formula: [-h*F + M] ./ Fz, h = 5 mm (plate surface to sensor height)
+copSlow(:, 2) = ...
+    (-5*grfDataSlow(:, 2) + grfDataSlow(:, 4)) ./ grfDataSlow(:, 3);
+copSlow(:, 1) = ...
+    (-5*grfDataSlow(:, 1) - grfDataSlow(:, 5)) ./ grfDataSlow(:, 3);
+copSlow(:, 3) = 0;
+copSlow = bsxfun(@plus, slowTransMat*copSlow', slowTransVec);
+fzSlow  = grfDataSlow(:, 3);
+
+%% Compute COP for Fast Leg
+copFast(:, 2) = ...
+    (-5*grfDataFast(:, 2) + grfDataFast(:, 4)) ./ grfDataFast(:, 3);
+copFast(:, 1) = ...
+    (-5*grfDataFast(:, 1) - grfDataFast(:, 5)) ./ grfDataFast(:, 3);
+copFast(:, 3) = 0;
+copFast = bsxfun(@plus, fastTransMat*copFast', fastTransVec);
+fzFast  = grfDataFast(:, 3);
+
+%% Compute Combined COP
+% Weighted average of each leg's COP by its vertical force
+COP = bsxfun(@rdivide, ...
+    bsxfun(@times, copFast, fzFast') + ...
+    bsxfun(@times, copSlow, fzSlow'), ...
+    fzSlow' + fzFast');
 
 end
 
