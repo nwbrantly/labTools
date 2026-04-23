5620b2c228250b7d3d3c879c5fcb2a48497066fa
nwb18 <nwbrantly@pitt.edu>
2026-04-23 11:22:10 -0400

    Reformat function
    Rename function variables using Claude Code to improve function readability, making it more explicit as to what each line of code does. Add clear section headers and code block comments. Update code whitespace for consistency and clarity. Considerably consolidate code by vectorizing COM computations. Update HAT COM computation code lines to clarify each operation. NWB

--- fun/parameterCalculation/COMCalculator.m
+++ fun/parameterCalculation/COMCalculator.m
@@ -1,91 +1,3 @@
-% Define the marker data
-if isempty(markerData.getLabelsThatMatch('HAT')) %we don't have the data we need to do this for realy, but we can use a proxy
-    warning('Not enough markers to calculate the COM, using the average of the hips as a proxy')
-    %THe average of the pelvis would be better, but people don't usually
-    %take the time to fill these markers, so this will be more robust
-    BCOM=[mean([RHip(:, 1) LHip(:, 1)], 2) mean([RHip(:, 2) LHip(:, 2)], 2) mean([RHip(:, 3) LHip(:, 3)], 2)];
-else
-
-    %get ankle position
-    RAnk=markerData.getDataAsVector({['RANK' orientation.sideAxis], ['RANK' orientation.foreaftAxis], ['RANK' orientation.updownAxis]});
-    RAnk=[orientation.sideSign*RAnk(:, 1), orientation.foreaftSign*RAnk(:, 2), orientation.updownSign*RAnk(:, 3)];
-    LAnk=markerData.getDataAsVector({['LANK' orientation.sideAxis], ['LANK' orientation.foreaftAxis], ['LANK' orientation.updownAxis]});
-    LAnk=[orientation.sideSign*LAnk(:, 1), orientation.foreaftSign*LAnk(:, 2), orientation.updownSign*LAnk(:, 3)];
-    %get knee position
-    RKnee=markerData.getDataAsVector({['RKNE' orientation.sideAxis], ['RKNE' orientation.foreaftAxis], ['RKNE' orientation.updownAxis]});
-    RKnee=[orientation.sideSign*RKnee(:, 1), orientation.foreaftSign*RKnee(:, 2), orientation.updownSign*RKnee(:, 3)];
-    LKnee=markerData.getDataAsVector({['LKNE' orientation.sideAxis], ['LKNE' orientation.foreaftAxis], ['LKNE' orientation.updownAxis]});
-    LKnee=[orientation.sideSign*LKnee(:, 1), orientation.foreaftSign*LKnee(:, 2), orientation.updownSign*LKnee(:, 3)];
-    %get toe position
-    RToe=markerData.getDataAsVector({['RTOE' orientation.sideAxis], ['RTOE' orientation.foreaftAxis], ['RTOE' orientation.updownAxis]});
-    RToe=[orientation.sideSign*RToe(:, 1), orientation.foreaftSign*RToe(:, 2), orientation.updownSign*RToe(:, 3)];
-    LToe=markerData.getDataAsVector({['LTOE' orientation.sideAxis], ['LTOE' orientation.foreaftAxis], ['LTOE' orientation.updownAxis]});
-    LToe=[orientation.sideSign*LToe(:, 1), orientation.foreaftSign*LToe(:, 2), orientation.updownSign*LToe(:, 3)];
-    %get HAT position
-    HAT=markerData.getDataAsVector({['HAT' orientation.sideAxis], ['HAT' orientation.foreaftAxis], ['HAT' orientation.updownAxis]});
-    HAT=[orientation.sideSign*HAT(:, 1), orientation.foreaftSign*HAT(:, 2), orientation.updownSign*HAT(:, 3)];
-
-    %% Need to calculate the position of the center of mass of each segment
-
-    %Foot:
-    fcomxR=abs(RAnk(:, 1)-RToe(:, 1)).*.5+RToe(:, 1); %m
-    fcomxL=abs(LAnk(:, 1)-LToe(:, 1)).*.5+LToe(:, 1); %m
-    fcomyR=abs(RAnk(:, 2)-RToe(:, 2)).*.5+RToe(:, 2); %m
-    fcomzR=abs(RAnk(:, 3)-RToe(:, 3)).*.5+RToe(:, 3); %m
-    fcomyL=abs(LAnk(:, 2)-LToe(:, 2)).*.5+LToe(:, 2); %m
-    fcomzL=abs(LAnk(:, 3)-LToe(:, 3)).*.5+LToe(:, 3); %m
-
-    fcomR=[fcomxR, fcomyR, fcomzR]; %foot
-    fcomL=[fcomxL, fcomyL, fcomzL];
-
-    %Shank: former I was using 0.394, but that is the whole leg, I just want
-    %the shank, 0.567 is closer to what I want but I need to recalculate
-    display('Updated Shank COM length?')
-    scomxR=abs(RKnee(:, 1)-RAnk(:, 1)).*.567+RAnk(:, 1);
-    scomxL=abs(LKnee(:, 1)-LAnk(:, 1)).*.567+LAnk(:, 1);
-    scomyR=abs(RKnee(:, 2)-RAnk(:, 2)).*.567+RAnk(:, 2);
-    scomzR=abs(RKnee(:, 3)-RAnk(:, 3)).*.567+RAnk(:, 3);
-    scomyL=abs(LKnee(:, 2)-LAnk(:, 2)).*.567+LAnk(:, 2);
-    scomzL=abs(LKnee(:, 3)-LAnk(:, 3)).*.567+LAnk(:, 3);
-
-    scomR=[scomxR, scomyR, scomzR]; %Shank
-    scomL=[scomxL, scomyL, scomzL];
-
-    %Thigh:
-    tcomxR=abs(RHip(:, 1)-RKnee(:, 1)).*.567+RKnee(:, 1);
-    tcomxL=abs(LHip(:, 1)-LKnee(:, 1)).*.567+LKnee(:, 1);
-    tcomyR=abs(RHip(:, 2)-RKnee(:, 2)).*.567+RKnee(:, 2);
-    tcomzR=abs(RHip(:, 3)-RKnee(:, 3)).*.567+RKnee(:, 3);
-    tcomyL=abs(LHip(:, 2)-LKnee(:, 2)).*.567+LKnee(:, 2);
-    tcomzL=abs(LHip(:, 3)-LKnee(:, 3)).*.567+LKnee(:, 3);
-
-    tcomR=[tcomxR, tcomyR, tcomzR]; %Thigh
-    tcomL=[tcomxL, tcomyL, tcomzL];
-
-    %HAT: head, arms, trunk
-    % The distal distance from top of the head I calculated useing winter and
-    % the midsized pilot from this document (http://www.smf.org/docs/articles/hic/USAARL_88-5.pdf)
-    HATcomx=abs(nanmean([RHip(:, 1) LHip(:, 1)], 2)-HAT(:, 1)).*(1-0.697)+nanmean([RHip(:, 1); LHip(:, 1)]);
-    HATcomy=abs(nanmean([RHip(:, 2) LHip(:, 2)], 2)-HAT(:, 2)).*(1-0.697)+nanmean([RHip(:, 2); LHip(:, 2)]);
-    HATcomz=abs(nanmean([RHip(:, 3) LHip(:, 3)], 2)-HAT(:, 3)).*(1-0.697)+nanmean([RHip(:, 3); LHip(:, 3)]);
-
-    HATcom=[HATcomx, HATcomy, HATcomz];
-
-    %% Need to compile whole body COM
-    FootW=0.0145.*BW;
-    shankW=0.0465.*BW;
-    thighW=0.1.*BW;
-    HATW=0.71.*BW;
-
-    BodyCOMx=(1/BW).*((fcomxR*FootW+scomxR*shankW+tcomxR*thighW)+(fcomxL*FootW+scomxL*shankW+tcomxL*thighW)+HATcomx*HATW);
-    BodyCOMy=(1/BW).*((fcomyR*FootW+scomyR*shankW+tcomyR*thighW)+(fcomyL*FootW+scomyL*shankW+tcomyL*thighW)+HATcomy*HATW);
-    BodyCOMz=(1/BW).*((fcomzR*FootW+scomzR*shankW+tcomzR*thighW)+(fcomzL*FootW+scomzL*shankW+tcomzL*thighW)+HATcomz*HATW);
-
-    BCOM=[BodyCOMx, BodyCOMy, BodyCOMz];
-    %% Save everything in an orientedLabTS
-    COMData=[fcomR fcomL scomR scomL tcomR tcomL HATcom BCOM]; %CJS note to self, change here to change what is stored in the COM
-    labels={'RfCOMx', 'RfCOMy', 'RfCOMz', 'LfCOMx', 'LfCOMy', 'LfCOMz', 'RsCOMx', 'RsCOMy', 'RsCOMz', 'LsCOMx', 'LsCOMy', 'LsCOMz', 'RtCOMx', 'RtCOMy', 'RtCOMz', 'LtCOMx', 'LtCOMy', 'LtCOMz', 'HATCOMx', 'HATCOMy', 'HATCOMz', 'BCOMx', 'BCOMy', 'BCOMz'};
-end
 function markerDataOut = COMCalculator(markerData, BW)
 % COMCalculator  Approximate whole-body center of mass from marker data.
 %
@@ -147,6 +59,115 @@ lHip = [orientation.sideSign    * lHip(:, 1), ...
     orientation.foreaftSign * lHip(:, 2), ...
     orientation.updownSign  * lHip(:, 3)];
 
+%% Compute Body Center of Mass
+if isempty(markerData.getLabelsThatMatch('HAT'))
+    % No HAT marker: use mean hip position as a proxy.
+    % NOTE: pelvis ASIS/PSIS markers would be more accurate, but hip
+    %   markers are more reliably present in typical datasets
+    warning('COMCalculator:insufficientMarkers', ...
+        ['Not enough markers to compute the COM; ' ...
+        'using average hip position as proxy.']);
+    bCOM = [mean([rHip(:, 1) lHip(:, 1)], 2), ...
+        mean([rHip(:, 2) lHip(:, 2)], 2), ...
+        mean([rHip(:, 3) lHip(:, 3)], 2)];
+
+else
+    %% Compute Segment Centers of Mass
+    % Retrieve segment marker data (ankle, knee, toe, HAT)
+    rAnk = markerData.getDataAsVector({ ...
+        ['RANK' orientation.sideAxis], ...
+        ['RANK' orientation.foreaftAxis], ...
+        ['RANK' orientation.updownAxis]});
+    rAnk = [orientation.sideSign    * rAnk(:, 1), ...
+        orientation.foreaftSign * rAnk(:, 2), ...
+        orientation.updownSign  * rAnk(:, 3)];
+    lAnk = markerData.getDataAsVector({ ...
+        ['LANK' orientation.sideAxis], ...
+        ['LANK' orientation.foreaftAxis], ...
+        ['LANK' orientation.updownAxis]});
+    lAnk = [orientation.sideSign    * lAnk(:, 1), ...
+        orientation.foreaftSign * lAnk(:, 2), ...
+        orientation.updownSign  * lAnk(:, 3)];
+    rKnee = markerData.getDataAsVector({ ...
+        ['RKNE' orientation.sideAxis], ...
+        ['RKNE' orientation.foreaftAxis], ...
+        ['RKNE' orientation.updownAxis]});
+    rKnee = [orientation.sideSign    * rKnee(:, 1), ...
+        orientation.foreaftSign * rKnee(:, 2), ...
+        orientation.updownSign  * rKnee(:, 3)];
+    lKnee = markerData.getDataAsVector({ ...
+        ['LKNE' orientation.sideAxis], ...
+        ['LKNE' orientation.foreaftAxis], ...
+        ['LKNE' orientation.updownAxis]});
+    lKnee = [orientation.sideSign    * lKnee(:, 1), ...
+        orientation.foreaftSign * lKnee(:, 2), ...
+        orientation.updownSign  * lKnee(:, 3)];
+    rToe = markerData.getDataAsVector({ ...
+        ['RTOE' orientation.sideAxis], ...
+        ['RTOE' orientation.foreaftAxis], ...
+        ['RTOE' orientation.updownAxis]});
+    rToe = [orientation.sideSign    * rToe(:, 1), ...
+        orientation.foreaftSign * rToe(:, 2), ...
+        orientation.updownSign  * rToe(:, 3)];
+    lToe = markerData.getDataAsVector({ ...
+        ['LTOE' orientation.sideAxis], ...
+        ['LTOE' orientation.foreaftAxis], ...
+        ['LTOE' orientation.updownAxis]});
+    lToe = [orientation.sideSign    * lToe(:, 1), ...
+        orientation.foreaftSign * lToe(:, 2), ...
+        orientation.updownSign  * lToe(:, 3)];
+    hatPos = markerData.getDataAsVector({ ...
+        ['HAT' orientation.sideAxis], ...
+        ['HAT' orientation.foreaftAxis], ...
+        ['HAT' orientation.updownAxis]});
+    hatPos = [orientation.sideSign    * hatPos(:, 1), ...
+        orientation.foreaftSign * hatPos(:, 2), ...
+        orientation.updownSign  * hatPos(:, 3)];
+
+    % Compute each segment COM as a fraction of the distal-to-proximal
+    % distance. Foot fraction = 0.5; shank/thigh = 0.567 (Winter tables).
+    % TODO: verify shank and thigh fractions; 0.567 may need recalculation
+    rFootCOM  = abs(rAnk  - rToe)  .* 0.5   + rToe;
+    lFootCOM  = abs(lAnk  - lToe)  .* 0.5   + lToe;
+    rShankCOM = abs(rKnee - rAnk)  .* 0.567 + rAnk;
+    lShankCOM = abs(lKnee - lAnk)  .* 0.567 + lAnk;
+    rThighCOM = abs(rHip  - rKnee) .* 0.567 + rKnee;
+    lThighCOM = abs(lHip  - lKnee) .* 0.567 + lKnee;
+
+    % HAT COM: 30.3% from the HAT marker toward the mean hip position
+    % (fraction 1 - 0.697 derived from Winter and USAARL-88-5 pilot data)
+    hipMidpoint = (rHip + lHip) ./ 2;
+    hipMeanPos  = nanmean([rHip; lHip]);  % grand mean of all hip samples
+    hatCOM = abs(hipMidpoint - hatPos) .* (1 - 0.697) + hipMeanPos;
+
+    %% Compile Whole-Body Center of Mass
+    % Segment mass fractions from Winter anthropometric tables
+    footWeight  = 0.0145 .* BW;
+    shankWeight = 0.0465 .* BW;
+    thighWeight = 0.1    .* BW;
+    hatWeight   = 0.71   .* BW;
+
+    rightLegCOM = rFootCOM*footWeight + rShankCOM*shankWeight + ...
+        rThighCOM*thighWeight;
+    leftLegCOM  = lFootCOM*footWeight + lShankCOM*shankWeight + ...
+        lThighCOM*thighWeight;
+    bodyCOM = (1/BW) .* ...
+        (rightLegCOM + leftLegCOM + hatCOM*hatWeight);
+    bCOM = bodyCOM;
+
+    % CJS note: change here to change what is stored in the COM dataset
+    segCOMData = [rFootCOM lFootCOM rShankCOM lShankCOM ...
+        rThighCOM lThighCOM hatCOM bodyCOM];
+    segLabels = { ...
+        'RfCOMx', 'RfCOMy', 'RfCOMz', ...
+        'LfCOMx', 'LfCOMy', 'LfCOMz', ...
+        'RsCOMx', 'RsCOMy', 'RsCOMz', ...
+        'LsCOMx', 'LsCOMy', 'LsCOMz', ...
+        'RtCOMx', 'RtCOMy', 'RtCOMz', ...
+        'LtCOMx', 'LtCOMy', 'LtCOMz', ...
+        'HATCOMx', 'HATCOMy', 'HATCOMz', ...
+        'BCOMx', 'BCOMy', 'BCOMz'};
+end
 
 %% Append Body COM to Marker Dataset
 labelsBody    = {'BCOMx', 'BCOMy', 'BCOMz'};
